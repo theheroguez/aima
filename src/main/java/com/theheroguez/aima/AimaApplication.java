c384ae0ac4c0d4c82d2a8de68359ce8de4b3861c
theheroguez <theheroguez@protonmail.com>
2024-02-20 18:58:38 -0600

    Create basic Spring Boot project structure
    This commit introduces a basic structure for a Spring Boot project. It creates the necessary Maven configuration files and wrapper scripts for building the application. It also adds an application properties file, a gitignore file, and template classes for a main application and a test case.

---
+++ src/main/java/com/theheroguez/aima/AimaApplication.java
@@ -0,0 +1,13 @@
+package com.theheroguez.aima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theheroguez/aima/AimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theheroguez.aima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
