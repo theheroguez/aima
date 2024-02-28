522d99c417280c85463a7f80276681ba4ca9a4e5
theheroguez <theheroguez@protonmail.com>
2024-02-27 21:37:46 -0600

    Refactor and simplify tabulation table and action rules
    TableConfiguration class has been refactored to simplify and make the code more readable. Function `tabulateTableForTDA` now uses new action names and function `rulesSimpleReflexAgent` uses a different approach for defining rules. These changes have improved the overall readability and maintainability of the codebase.

--- src/main/java/com/theheroguez/aima/utils/TableConfiguration.java
+++ src/main/java/com/theheroguez/aima/utils/TableConfiguration.java
@@ -1,13 +1,15 @@
 package com.theheroguez.aima.utils;
 
-import com.theheroguez.aima.enums.Action;
+import com.theheroguez.aima.*;
 import com.theheroguez.aima.enums.Location;
 import com.theheroguez.aima.enums.Status;
-import com.theheroguez.aima.percepts.Percept;
+import com.theheroguez.aima.environment.vacuum.VacuumState;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.*;
+
+import static com.theheroguez.aima.environment.vacuum.VacuumTableDrivenAgent.*;
 
 /**
  * The TableConfiguration class is a configuration class that provides a bean for tabulating a table for TDA (Table-Driven Agent).
@@ -15,41 +17,39 @@ import java.util.*;
  */
 @Configuration
 public class TableConfiguration {
-
     @Bean
-    public Map<List<Percept<?, ?>>, Action> tabulateTableForTDA() {
-        Map<List<Percept<?, ?>>, Action> table = new HashMap<>();
-        table.put(Collections.singletonList(new Percept<>(Location.A, Status.CLEAN)), Action.RIGHT);
-        table.put(Collections.singletonList(new Percept<>(Location.A, Status.DIRTY)), Action.CLEAN);
-        table.put(Collections.singletonList(new Percept<>(Location.B, Status.CLEAN)), Action.LEFT);
-        table.put(Collections.singletonList(new Percept<>(Location.B, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.A, Status.CLEAN)), Action.RIGHT);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.A, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.A, Status.CLEAN)), Action.RIGHT);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.A, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.B, Status.CLEAN)), Action.RIGHT);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.B, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.B, Status.CLEAN)), Action.LEFT);
-        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.B, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.A, Status.CLEAN)), Action.LEFT);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.A, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.A, Status.CLEAN)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.A, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.B, Status.CLEAN)), Action.LEFT);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.B, Status.DIRTY)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.B, Status.CLEAN)), Action.CLEAN);
-        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.B, Status.DIRTY)), Action.CLEAN);
+    public Map<List<Percept<Location, Status>>, Action> tabulateTableForTDA() {
+        Map<List<Percept<Location, Status>>, Action> table = new HashMap<>();
+        table.put(Collections.singletonList(new Percept<>(Location.A, Status.CLEAN)), MOVE_RIGHT);
+        table.put(Collections.singletonList(new Percept<>(Location.A, Status.DIRTY)), SUCK);
+        table.put(Collections.singletonList(new Percept<>(Location.B, Status.CLEAN)), MOVE_LEFT);
+        table.put(Collections.singletonList(new Percept<>(Location.B, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.A, Status.CLEAN)), MOVE_RIGHT);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.A, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.A, Status.CLEAN)), MOVE_RIGHT);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.A, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.B, Status.CLEAN)), MOVE_RIGHT);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.CLEAN), new Percept<>(Location.B, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.B, Status.CLEAN)), MOVE_LEFT);
+        table.put(Arrays.asList(new Percept<>(Location.A, Status.DIRTY), new Percept<>(Location.B, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.A, Status.CLEAN)), MOVE_LEFT);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.A, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.A, Status.CLEAN)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.A, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.B, Status.CLEAN)), MOVE_LEFT);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.CLEAN), new Percept<>(Location.B, Status.DIRTY)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.B, Status.CLEAN)), SUCK);
+        table.put(Arrays.asList(new Percept<>(Location.B, Status.DIRTY), new Percept<>(Location.B, Status.DIRTY)), SUCK);
 
         return table;
     }
 
     @Bean
-    public Map<Percept<?, ?>, Action> rulesSimpleReflexAgent() {
-        Map<Percept<?, ?>, Action> rules = new HashMap<>();
-        rules.put(new Percept<>(Location.A, Status.CLEAN), Action.RIGHT);
-        rules.put(new Percept<>(Location.A, Status.DIRTY), Action.CLEAN);
-        rules.put(new Percept<>(Location.B, Status.CLEAN), Action.LEFT);
-        rules.put(new Percept<>(Location.B, Status.DIRTY), Action.CLEAN);
+    public List<Rule<Action>> simpleReflexRules() {
+        List<Rule<Action>> rules = new ArrayList<>();
+        rules.add(new Rule<>(SUCK, state -> ((VacuumState) state).getCurrentState().equals(Status.DIRTY)));
+        rules.add(new Rule<>(MOVE_RIGHT, state -> ((VacuumState) state).getCurrentState().equals(Status.CLEAN) && ((VacuumState) state).getCurrentLocation().equals(Location.A)));
+        rules.add(new Rule<>(MOVE_LEFT, state -> ((VacuumState) state).getCurrentState().equals(Status.CLEAN) && ((VacuumState) state).getCurrentLocation().equals(Location.B)));
 
         return rules;
     }
