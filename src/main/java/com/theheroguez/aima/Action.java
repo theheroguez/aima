95a06895618df788da9539ba9a90c0399fd2e1c9
theheroguez <theheroguez@protonmail.com>
2024-02-27 21:28:55 -0600

    Add Action interface file with JavaDoc comments
    A new file called Action.java has been added under the 'aima' package. This file represents an 'Action' interface for an agent and contains necessary JavaDoc comments. Instructions have been added for implementing classes to override the toString() method for a human-readable description of the action.

---
+++ src/main/java/com/theheroguez/aima/Action.java
@@ -0,0 +1,9 @@
+package com.theheroguez.aima;
+
+/**
+ * The Action interface represents an action that can be performed by an agent in a particular problem domain.
+ * Actions can be defined as classes that implement this interface.
+ * Implementing classes should override the toString() method to provide a human-readable representation of the action.
+ */
+public interface Action {
+}
