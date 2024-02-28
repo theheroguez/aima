130ecd31b1ef2a80e7ca74650de2bdc75f27886f
theheroguez <theheroguez@protonmail.com>
2024-02-27 21:38:45 -0600

    Refactor SimpleReflexAgent class
    The SimpleReflexAgent class was refactored to replace usage of the Action enumeration and Percept interface with Rule and State classes for actions determination. This resulted in a cleaner and more abstract structure, more conducive to code maintainability. Additionally, some function definitions were altered to better reflect the new structure.

--- src/main/java/com/theheroguez/aima/agents/SimpleReflexAgent.java
+++ src/main/java/com/theheroguez/aima/agents/SimpleReflexAgent.java
@@ -1,21 +1,19 @@
 package com.theheroguez.aima.agents;
 
-import com.theheroguez.aima.enums.Action;
-import com.theheroguez.aima.percepts.Percept;
-import org.springframework.stereotype.Component;
+import com.theheroguez.aima.Rule;
+import com.theheroguez.aima.State;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 /**
  * The SimpleReflexAgent class represents a simple reflex agent that uses a set of rules to determine the appropriate action based on a given percept.
  */
-@Component
-public class SimpleReflexAgent implements Agent {
 
-    private final Map<Percept<?, ?>, Action> rules;
+public abstract class SimpleReflexAgent<P, A> implements Agent<P, A> {
+    protected final List<Rule<A>> rules;
 
-    public SimpleReflexAgent(Map<Percept<?, ?>, Action> rules) {
+    public SimpleReflexAgent(List<Rule<A>> rules) {
         this.rules = rules;
     }
 
@@ -26,12 +24,15 @@ public class SimpleReflexAgent implements Agent {
      * @return the action determined by the rules
      */
     @Override
-    public Action process(Percept<?, ?> percept) {
-        return rules.get(percept);
+    public Optional<A> apply(P percept) {
+        State state = interpretInput(percept);
+        Rule<A> rule = ruleMatch(state);
+        return Optional.ofNullable(rule.getAction());
     }
 
-    @Override
-    public Action lookUp(List<Percept<?, ?>> percepts, Map<List<Percept<?, ?>>, Action> table) {
-        return null;
+    protected abstract State interpretInput(P percept);
+
+    private Rule<A> ruleMatch(State state) {
+        return rules.stream().filter(r -> r.evaluate(state)).findFirst().orElse(null);
     }
 }
